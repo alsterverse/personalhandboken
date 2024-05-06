9eafa6d4fa1dbbc661f8f49067ad51e01634fa17
Jim Löfgren <jim.lofgren@alster.se>
2024-05-06 16:06:26 +0200

    added chat url

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -20,8 +20,24 @@ const app = new App({
     receiver: expressReceiver
 });
 
-app.message(async ({ say }) => {
-    await say("Hello, world!");
+app.message(async ({ message, say }) => {
+    try {
+      let text = message.text
+      const res = await fetch(process.env.CHAT_URL, {
+            method: 'POST',
+            body: JSON.stringify(
+                {"messages": [{"content": text}]}
+            )
+        })
+        
+        const answer = await res.text()
+
+        say(answer)
+   
+    } catch (error) {
+      console.log("err")
+      console.error(error);
+    }
 });
 
 exports.handler = async (event, context) => {
