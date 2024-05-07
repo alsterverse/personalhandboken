510e4f535d3b5b407c88efc64c62cb751c75ef8b
Jim Löfgren <jim.lofgren@alster.se>
2024-05-07 09:31:54 +0200

    await

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -25,7 +25,7 @@ app.message(async ({ message, say }) => {
     try {
       let text = message.text
 
-      text = getOpenAiResponse("text")
+      text = await getOpenAiResponse("text")
     //   const res = await fetch(process.env.CHAT_URL, {
     //         method: 'POST',
     //         body: JSON.stringify(
